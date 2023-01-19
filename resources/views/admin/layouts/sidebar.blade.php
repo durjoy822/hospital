0bacddb94a8380d8d69d2bd2c47edee9b619bd5a
Saboj Kumar Shaha <sahaapo@gmail.com>
2023-01-19 10:24:56 +0600

    sidebar updated

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -76,97 +76,10 @@
                 </li>
             </ul>
         </li>
-        <li class="nav-level-one">
-            <a href="#nav-uiKit" data-toggle="collapse" aria-expanded="false">
-                <span class="ti-layout-tab"></span> UI Kit
-            </a>
-            <ul class="list-unstyled collapse" aria-expanded="true" id="nav-uiKit">
-                <li>
-                    <a href="typography.html">Typography</a>
-                </li>
-                <li>
-                    <a href="buttons.html">Buttons</a>
-                </li>
-                <li>
-                    <a href="cards.html">Cards</a>
-                </li>
-                <li>
-                    <a href="tabs.html">Tabs</a>
-                </li>
-                <li>
-                    <a href="accordions.html">Accordions</a>
-                </li>
-                <li>
-                    <a href="modals.html">Modals</a>
-                </li>
-                <li>
-                    <a href="lists.html">Lists &amp; Media Object</a>
-                </li>
-                <li>
-                    <a href="grid.html">Grid</a>
-                </li>
-                <li>
-                    <a href="progress-bars.html">Progress Bars</a>
-                </li>
-                <li>
-                    <a href="notifications-alerts.html">Notifications &amp; Alerts</a>
-                </li>
-                <li>
-                    <a href="pagination.html">Pagination</a>
-                </li>
-                <li>
-                    <a href="carousel.html">Carousel</a>
-                </li>
-            </ul>
-        </li>
         <li>
             <a href="{{route('department.index')}}">
                 <span class="ti-layout-menu-v"></span> Department
             </a>
-        </li>
-        <li>
-            <a href="#nav-charts" data-toggle="collapse" aria-expanded="false">
-                <span class="ti-pie-chart"></span> Charts
-            </a>
-            <ul class="collapse list-unstyled" id="nav-charts">
-                <li>
-                    <a href="charts-1.html">Morris</a>
-                </li>
-                <li>
-                    <a href="charts-2.html">Flot</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="#nav-maps" data-toggle="collapse" aria-expanded="false">
-                <span class="ti-location-pin"></span> Maps
-            </a>
-            <ul class="collapse list-unstyled" id="nav-maps">
-                <li>
-                    <a href="map-1.html">Google Maps</a>
-                </li>
-                <li>
-                    <a href="map-2.html">Vector Maps</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="forms.html">
-                <span class="ti-layout-media-overlay-alt-2"></span> Forms
-            </a>
-        </li>
-        <li>
-            <a href="#nav-icons" data-toggle="collapse" aria-expanded="false">
-                <span class="ti-themify-favicon"></span> icons
-            </a>
-            <ul class="collapse list-unstyled" id="nav-icons">
-                <li>
-                    <a href="font-awesome.html">Font Awesome </a>
-                </li>
-                <li>
-                    <a href="themify.html">Themify</a>
-                </li>
-            </ul>
         </li>
         <li>
             <a href="#nav-pages" data-toggle="collapse" aria-expanded="false">
